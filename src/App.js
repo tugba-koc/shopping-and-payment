ff9b0d4e9cfcc2a80b261a03957e6f967181c54c
frknkoc <furkan.koc.488@gmail.com>
2022-04-09 15:08:43 +0300

    setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import LoginForm from './components/LoginForm/LoginForm';
-import "./App.scss"
+import Packets from './components/Packets/Packets';
+import "./App.scss";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <LoginForm />
-    
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginForm />}> </Route>
+          <Route path="/packets" element={<Packets />}> </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
